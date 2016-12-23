2f10b3c7085c67c1737d6f291e489ff7ddc93bc5
Chuckie Chang <546007561@qq.com>
2016-12-23 17:48:54 +0800

    Update

--- uix-slides.php
+++ uix-slides.php
@@ -121,12 +121,17 @@ class UixSlides {
 	 *
 	 */
 	public static function inc_str( $str, $incstr ) {
-	
-		if ( mb_strlen( strpos( $str, $incstr ), 'UTF8' ) > 0 ) {
-			return true;
+	    
+		if ( !empty( $incstr ) ) {
+			if ( preg_match( '/'.$incstr.'/', $str ) ) {
+				return true;
+			} else {
+				return false;
+			}
 		} else {
 			return false;
 		}
+
 
 	}
 
